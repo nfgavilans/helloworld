e531135a48b715216acde5a6f89b90b03e8529d6
unknown <nfgavilans@unal.edu.co>
2020-11-27 22:06:18 -0500

    Prueba de autorecarga con H1

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />GIT 
-        <p>
-          HOLA, FUNCIONA DESGRACIADO
-        </p>
+        
+        <h1>HOLA, FUNCIONA DESGRACIADO</h1>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
